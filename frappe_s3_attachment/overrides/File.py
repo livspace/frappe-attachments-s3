5a2ca1ceb5ef464ec179b5c8e1c601871371db8a
praveen-livspace <praveen@livspace.com>
2025-01-25 11:29:37 +0530

    Override get_full_path to handle S3 URLs

--- frappe_s3_attachment/overrides/File.py
+++ frappe_s3_attachment/overrides/File.py
@@ -1,5 +1,6 @@
 import frappe
 from frappe.core.doctype.file.file import File
+from frappe.utils import get_url
 
 class FileOverride(File):
     def validate_file_url(self):
@@ -12,3 +13,11 @@ class FileOverride(File):
                 _("URL must start with http:// or https://"),
                 title=_("Invalid URL"),
             )
+    def get_full_path(self):
+
+        site_url = get_url()
+
+        if file_path.startswith("/api/method/frappe_s3_attachment.controller.generate_file"):
+            return site_url + file_path
+
+        return super().get_full_path()
