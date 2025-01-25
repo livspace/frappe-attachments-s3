f6bc3ccc1f7273e6051bc6a3bd25f6c33ea3e401
praveen-livspace <praveen@livspace.com>
2025-01-25 13:34:54 +0530

    Override get_content for s3 files

--- frappe_s3_attachment/overrides/File.py
+++ frappe_s3_attachment/overrides/File.py
@@ -1,25 +1,37 @@
 import frappe
 from frappe.core.doctype.file.file import File
 from frappe.utils import get_url
+from urllib.parse import urlparse, parse_qs
+
+from frappe_s3_attachment.controller import S3Operations
+
 
 class FileOverride(File):
-    def validate_file_url(self):
-        if self.is_remote_file or not self.file_url:
-            return
-
-        if not self.file_url.startswith(("/files/", "/private/files/", "/api/method/frappe_s3_attachment.controller.generate_file")):
-            # Probably an invalid URL since it doesn't start with http either
-            frappe.throw(
-                _("URL must start with http:// or https://"),
-                title=_("Invalid URL"),
-            )
-    def get_full_path(self):
-
-        site_url = get_url()
-
+    def is_s3file(self):
         file_path = self.file_url or self.file_name
+        return not self.is_folder and file_path.startswith("/api/method/frappe_s3_attachment.controller.generate_file")
 
-        if file_path.startswith("/api/method/frappe_s3_attachment.controller.generate_file"):
+    def validate_file_url(self):
+        if self.is_s3file:
+            return
+        super().validate_file_url()
+
+    def get_full_path(self):
+        file_path = self.file_url or self.file_name
+        if self.is_s3file:
+            site_url = get_url()
             return site_url + file_path
 
         return super().get_full_path()
+
+    def get_content(self, encodings=None) -> bytes | str:
+
+        file_path = self.file_url or self.file_name
+        if self.is_s3file():
+            s3ops = S3Operations()
+            site_url = get_url()
+            parsed_url = urlparse(site_url + file_path)
+            s3key = parse_qs(parsed_url.query).get("key")
+            return s3ops.read_file_from_s3(s3key)
+
+        return super().get_content(encodings)
